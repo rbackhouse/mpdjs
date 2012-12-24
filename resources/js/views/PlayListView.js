b7f5b273830e20bf8b31b3befc6bf35681c6df21
Richard Backhouse <richardabackhouse@gmail.com>
2012-12-23 22:18:35 -0500

    More updates

--- resources/js/views/PlayListView.js
+++ resources/js/views/PlayListView.js
@@ -15,32 +15,72 @@ function($, Backbone, _, PlayList, mobile, template){
 				this.sendControlCmd("next");
 			},
 			"click #playPause" : function() {
-				this.sendControlCmd("play");
+				if (this.state === "play") {
+					this.sendControlCmd("pause");
+				} else {
+					this.sendControlCmd("play");
+				}
 			},
 			"click #stop" : function() {
 				this.sendControlCmd("stop");
 			},
+			"click #editButton" : "editPlayList",
 			"click #randomButton" : "randomPlayList",
 			"click #clearButton" : "clearPlayList",
+			"click #playingList li" : "removeSong",
 			"change #volume" : "changeVolume"
 		},
 		initialize: function(options) {
 			this.playlist = options.playlist;
 			this.template = _.template( template, { playlist: options.playlist.toJSON() } );
+        	$.ajax({
+        		url: "./music/status",
+        		type: "GET",
+	        	contentTypeString: "application/x-www-form-urlencoded; charset=utf-8",
+	        	dataType: "text",
+	        	success: function(data, textStatus, jqXHR) {
+		        	this.showStatus(data, true);
+	        	}.bind(this),
+	        	error: function(jqXHR, textStatus, errorThrown) {
+					console.log("status error : "+textStatus);
+	        	}
+        	});
 		},
 		render: function(){
 			$(this.el).html( this.template );
+		},
+		editPlayList: function() {
+			$("#playingList li").remove();
+			if (this.editing) {
+				this.editing = undefined;
+				$("#editButton .ui-btn-text").html("Edit");
+				this.playlist.each(function(song) {
+					$("#playingList").append('<li>'+song.get("artist")+' : '+song.get("title")+'</li>');	
+				});
+			} else {
+				this.editing = true;
+				$("#editButton .ui-btn-text").html("Done");
+				this.playlist.each(function(song) {
+					//$("#playingList").append('<li id="'+song.get("id")+'"><img src="images/minus.png" alt="Delete" class="ui-li-icon">'+song.get("artist")+' : '+song.get("title")+'</li>');	
+					$("#playingList").append('<li data-icon="minusIcon"><a href="#playlist" id="'+song.get("id")+'">'+song.get("artist")+' : '+song.get("title")+'</a></li>');	
+					//$("#playingList").append('<li><a href="#playlist" id="'+song.get("id")+'"><img src="images/minus.png" alt="Delete" class="ui-li-icon">'+song.get("artist")+' : '+song.get("title")+'</a></li>');	
+				});
+			}
+			$("#playingList").listview('refresh');
 		},
 		randomPlayList: function() {
         	$.ajax({
         		url: "./music/playlist/random",
         		type: "PUT",
+				headers: { "cache-control": "no-cache" },
 	        	contentTypeString: "application/x-www-form-urlencoded; charset=utf-8",
 	        	dataType: "text",
 	        	success: function(data, textStatus, jqXHR) {
-		        	this.fetchPlayList(data);
+		        	this.showStatus(data, true);
+		        	this.fetchPlayList();
 	        	}.bind(this),
 	        	error: function(jqXHR, textStatus, errorThrown) {
+					console.log("random playlist error : "+textStatus);
 	        	}
         	});
 		},
@@ -48,37 +88,54 @@ function($, Backbone, _, PlayList, mobile, template){
         	$.ajax({
         		url: "./music/playlist",
         		type: "DELETE",
+				headers: { "cache-control": "no-cache" },
 	        	contentTypeString: "application/x-www-form-urlencoded; charset=utf-8",
 	        	dataType: "text",
 	        	success: function(data, textStatus, jqXHR) {
-		        	this.fetchPlayList(data);
+		        	this.showStatus(data, true);
+		        	this.fetchPlayList();
 	        	}.bind(this),
 	        	error: function(jqXHR, textStatus, errorThrown) {
+					console.log("clear playlist error : "+textStatus);
 	        	}
         	});
 		},
-		fetchPlayList: function(data) {
-			if (data) {
-				var status = JSON.parse(data);
-				var volume = parseInt(status.volume);
-				if (volume > -1) {
-					$("#volume").val(status.volume);
-				}
-				if (status.currentsong) {
-					$("#currentlyPlaying").text("Currently Playing ["+currentsong+"]");
-				}
+		removeSong: function(evt) {
+			if (this.editing) {			
+				$.ajax({
+					url: "./music/playlist/"+evt.target.id,
+					type: "DELETE",
+					headers: { "cache-control": "no-cache" },
+					contentTypeString: "application/x-www-form-urlencoded; charset=utf-8",
+					dataType: "text",
+					success: function(data, textStatus, jqXHR) {
+		        		this.showStatus(data, true);
+						this.fetchPlayList();
+					}.bind(this),
+					error: function(jqXHR, textStatus, errorThrown) {
+						console.log("remove song error : "+textStatus);
+					}
+				});
 			}
+		},
+		fetchPlayList: function() {
 			this.playlist.fetch({
 				success: function(collection, response, options) {
 					this.playlist.reset(collection.toJSON());
 					$("#playingList li").remove();
 					this.playlist.each(function(song) {
-						$("#playingList").append('<li data-icon="delete"><a href="#playlist/remove/'+song.get("id")+'">'+song.get("artist")+' : '+song.get("title")+'</a></li>');	
-					});
+						if (this.editing) {
+							//$("#playingList").append('<li id="'+song.get("id")+'"><img src="images/minus.png" alt="Delete" class="ui-li-icon">'+song.get("artist")+' : '+song.get("title")+'</li>');	
+							$("#playingList").append('<li data-icon="minusIcon"><a href="#playlist" id="'+song.get("id")+'">'+song.get("artist")+' : '+song.get("title")+'</a></li>');	
+							//$("#playingList").append('<li><a href="#playlist" id="'+song.get("id")+'"><img src="images/minus.png" alt="Delete" class="ui-li-icon">'+song.get("artist")+' : '+song.get("title")+'</a></li>');	
+						} else {
+							$("#playingList").append('<li>'+song.get("artist")+' : '+song.get("title")+'</li>');	
+						}
+					}.bind(this));
 					$("#playingList").listview('refresh');
 				}.bind(this),
-				error: function(collection, xhr, options) {
-					console.log("failed!!!");
+				error: function(jqXHR, textStatus, errorThrown) {
+					console.log("fetch playlist error : "+textStatus);
 				}
 			});
 		},
@@ -86,11 +143,14 @@ function($, Backbone, _, PlayList, mobile, template){
         	$.ajax({
         		url: "./music/volume/"+$("#volume").val(),
         		type: "POST",
+				headers: { "cache-control": "no-cache" },
 	        	contentTypeString: "application/x-www-form-urlencoded; charset=utf-8",
 	        	dataType: "text",
 	        	success: function(data, textStatus, jqXHR) {
+	        		this.showStatus(data);
 	        	}.bind(this),
 	        	error: function(jqXHR, textStatus, errorThrown) {
+	        		console.log("change volume error: "+textStatus);
 	        	}
         	});
 		},
@@ -99,14 +159,42 @@ function($, Backbone, _, PlayList, mobile, template){
         	$.ajax({
         		url: "./music/"+type,
         		type: "POST",
+				headers: { "cache-control": "no-cache" },
 	        	contentTypeString: "application/x-www-form-urlencoded; charset=utf-8",
 	        	dataType: "text",
 	        	success: function(data, textStatus, jqXHR) {
-	        		
+	        		this.showStatus(data, true);
 	        	}.bind(this),
 	        	error: function(jqXHR, textStatus, errorThrown) {
+	        		console.log("control cmd error: "+textStatus);
 	        	}
         	});
+		},
+		showStatus: function(data, refreshVolume) {
+			var status = JSON.parse(data);
+			this.state = status.state;
+			console.log("state:"+status.state);
+			if (status.state === "play") {
+				//$("#playPause .ui-btn-text").html("Pause");
+				$("#playPause").buttonMarkup({icon : "pauseIcon" });
+			} else {
+				//$("#playPause .ui-btn-text").html("Play");
+				$("#playPause").buttonMarkup({icon : "playIcon" });
+			}
+			if (refreshVolume) {
+			var volume = parseInt(status.volume);
+				if (volume > -1) {
+					$("#volume").val(status.volume);
+				} else {
+					$("#volume").val("0");
+				}
+				$("#volume").slider('refresh');
+			}
+			if (status.currentsong && status.state === "play") {
+				$("#currentlyPlaying").text("Playing ["+status.currentsong+"]");
+			} else {
+				$("#currentlyPlaying").text("Playing []");
+			}
 		}
 	});
 	
